618cc2c154b75c8346a4d460deedc4acd67176a6
Mohammed Badran <mebadran@gmail.com>
2015-12-08 09:43:54 +1100

    improved build script

--- build.sh
+++ build.sh
@@ -1,13 +1,22 @@
 #!/bin/sh
 
+# update the base accordingly
+BASE=/Users/mo/Documents/projects/romanapi
+
 # cd into the code directory
-cd /Users/mo/Documents/projects/romanapi.com/src
+cd $BASE/src
+
+# create the deploy dir if it doesn't already exist
+mkdir -p $BASE/deploy
+
+# create a random identifier
+ID=$RANDOM
 
 # create the code archive
-zip -r /Users/mo/Documents/projects/romanapi.com/deploy/romanapi.zip .
+zip -r $BASE/deploy/romanapi_$ID.zip .
 
 # update the function on lambda and publish a new version
-aws --region us-east-1 lambda update-function-code --function-name arn:aws:lambda:us-east-1:956618040296:function:roman_api_convert --zip-file fileb:///Users/mo/Documents/projects/romanapi.com/deploy/romanapi.zip --publish
+aws --region us-east-1 lambda update-function-code --function-name arn:aws:lambda:us-east-1:956618040296:function:roman_api_convert --zip-file fileb://$BASE/deploy/romanapi_$ID.zip --publish
 
 # aliases
 # aws --region us-east-1 lambda list-aliases --function-name arn:aws:lambda:us-east-1:956618040296:function:roman_api_convert
